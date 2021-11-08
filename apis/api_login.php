276129069ea3c9074167accb513cdbb1cb1c601f
Lisa Søndergaard <lisa3074@stud.kea.dk>
2021-11-08 11:10:46 +0100

    added new db user and changed it in the script. Password is the same

--- apis/api_login.php
+++ apis/api_login.php
@@ -2,6 +2,7 @@
 require_once(__DIR__.'/../db.php');
 //Make sure that cookies can only be accessed in http requests (not JS), which prevents CSRF
 ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
 
 if(!isset($_SESSION)){
   session_start();
--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
  require_once(__DIR__.'/pass/pass.php');
 try{
-  $sDatabaseUserName = 'sheclmzz_lisa';
+  $sDatabaseUserName = 'sheclmzz_its_ma2';
   $sDatabasePassword = $pass;
   $sDatabaseConnection = "mysql:host=premium42.web-hosting.com; dbname=sheclmzz_its_mandatory2; charset=utf8mb4";
   $aDatabaseOptions = array(
@@ -12,4 +12,18 @@ try{
 }catch( PDOException $e){
   echo '{status:0, message: cannot connect to database} '.$e;
   exit();
-} 
+}  
+
+/*  try{
+  $sDatabaseUserName = 'sheclmzz_web_dev';
+  $sDatabasePassword = $pass;
+  $sDatabaseConnection = "mysql:host=premium42.web-hosting.com; dbname=sheclmzz_its_exam; charset=utf8mb4";
+  $aDatabaseOptions = array(
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  );
+  $db = new PDO( $sDatabaseConnection, $sDatabaseUserName, $sDatabasePassword, $aDatabaseOptions );
+}catch( PDOException $e){
+  echo '{status:0, message: cannot connect to database} '.$e;
+  exit();
+} */
