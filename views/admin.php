3ba9b71e34f8389e18a95929cbd135b5e3a5ab8f
Lisa Søndergaard <lisa3074@stud.kea.dk>
2021-11-10 09:53:32 +0100

    forgot a coulpe of outs on firstname/lastname to prevent xxs

--- views/admin.php
+++ views/admin.php
@@ -15,45 +15,45 @@ if (!isset($_SESSION)) {
 <main class="admin_main">
 
     <?php
-if (isset($display_message)) {?>
+    if (isset($display_message)) { ?>
     <p class="url_decode admin">
         <?php
-    // Use out to sanitize the read from the db, to prevent XXS 
-  echo out(urldecode($display_message));
-}?>
+        // Use out to sanitize the read from the db, to prevent XXS 
+        echo out(urldecode($display_message));
+    } ?>
     </p>
     <?php
-   
-if (isset($note)) {
-?>
+
+        if (isset($note)) {
+        ?>
     <p>Saved note:
         <?php
-      echo urldecode($note);
-    } ?>
+            echo urldecode($note);
+        } ?>
     </p>
 
     <section class="comment_wrapper">
 
         <article>
             <?php
-    foreach ($comments as $key => $comment) {
-      $isMe = $comment['user_id'] == $_SESSION['uuid'];
-      $comment_id = $comment['comment_id'];
-      $user_id = $comment['user_id'];  
-    ?>
-            <h1 class="heading"><?= $key === 0 ? 'Question:' : ''?><?= $key === 1 ? 'Answers:' : ''?> </h1>
-            <div class="comment <?= $isMe ? 'me' : ''?> <?= $key === 0 ? 'question' : ''?>">
+                    foreach ($comments as $key => $comment) {
+                        $isMe = $comment['user_id'] == $_SESSION['uuid'];
+                        $comment_id = $comment['comment_id'];
+                        $user_id = $comment['user_id'];
+                    ?>
+            <h1 class="heading"><?= $key === 0 ? 'Question:' : '' ?><?= $key === 1 ? 'Answers:' : '' ?> </h1>
+            <div class="comment <?= $isMe ? 'me' : '' ?> <?= $key === 0 ? 'question' : '' ?>">
                 <div class="flex">
-                    <h4><?= $isMe ? 'You' : $comment['firstname'] . ' ' . $comment['lastname']; ?></h4>
+                    <h4><?= $isMe ? 'You' : out($comment['firstname']) . ' ' . out($comment['lastname']); ?></h4>
                     <p><?= $comment['comment_ts'] ?></p>
                 </div>
                 <!-- Use out to sanitize the read from the db, to prevent XXS -->
-                <p class="<?= $key === 0 ? 'bold' : ''?>"><?= out($decrypted_comments[$key]); ?></p>
-                <?= $isMe ? "<form action='/admin/delete/$comment_id/$user_id' method='POST'><button>Delete</button></form>" : ''?>
+                <p class="<?= $key === 0 ? 'bold' : '' ?>"><?= out($decrypted_comments[$key]); ?></p>
+                <?= $isMe ? "<form action='/admin/delete/$comment_id/$user_id' method='POST'><button>Delete</button></form>" : '' ?>
             </div>
             <?php
-    }
-    ?>
+                    }
+                    ?>
         </article>
     </section>
     <form action="/admin/comment"
@@ -61,7 +61,7 @@ if (isset($note)) {
         <!-- Hidden input field to prevent CSRF with value that coresponds to the session['csrf] -->
         <input type="hidden"
             name="csrf"
-            value="<?= $_SESSION['csrf']?>">
+            value="<?= $_SESSION['csrf'] ?>">
         <textarea name="comment_text"
             id="comment"
             placeholder="Write a comment"></textarea>
@@ -71,7 +71,7 @@ if (isset($note)) {
         method="POST">
         <input type="hidden"
             name="csrf_secret"
-            value="<?= $_SESSION['csrf']?>">
+            value="<?= $_SESSION['csrf'] ?>">
         <textarea placeholder="Save secret note to self"
             name="note"></textarea>
         <button>Save</button>
@@ -83,4 +83,4 @@ if (isset($note)) {
 
 
 <?php
-  require_once(__DIR__ . '/bottom.php');
+require_once(__DIR__ . '/bottom.php');
